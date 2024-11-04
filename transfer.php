4b1920d36d31e8da5b576bf751acdecc29a9ba39
Dhruv Taraviya <dhruvtaraviya1@gmail.com>
2024-11-04 11:20:41 +0530

    Floder added

--- transfer.php
+++ transfer.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['Acc_No'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Money Transfer - Bank Management System</title>
-    <link rel="stylesheet" href="style5.css">
+    <link rel="stylesheet" href="css/style5.css">
 </head>
 <body>
     <header>
